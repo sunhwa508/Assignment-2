ec9c3393b80734f91dc3f1a1074f32da2ffbbb07
sunhwa <2017sunhwa@gmail.com>
2021-07-29 23:47:21 +0900

    ✨[feat]components 기본 구성 세팅 및 라우터 작업

---
+++ src/components/RecentList/recentList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "../../assets/data";
+import { Link } from "react-router-dom";
+
+class RecentList extends React.Component {
+	render() {
+		return (
+			<>
+				{data.map(item => (
+					<>
+						<Link to={`/product/${item.id}`}>{item.title}</Link>
+						<br />
+					</>
+				))}
+			</>
+		);
+	}
+}
+
+export { RecentList };
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RecentList } from "../../components";
+
+class HomePage extends React.Component {
+	render() {
+		return (
+			<div>
+				<RecentList />
+			</div>
+		);
+	}
+}
+
+export default HomePage;
